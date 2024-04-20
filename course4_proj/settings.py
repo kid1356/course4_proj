c5c85d6dcb86e58e81c78bfc749e058fbc7b4812
Adeel Khan <engr.adeelkhan2002@gmail.com>
2024-04-20 13:13:38 +0000

    Finish celery introduction

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -50,6 +50,7 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'movies',
         'gh',
+        'django_celery_results',
     ]
 
     MIDDLEWARE = [
@@ -81,6 +82,9 @@ class Dev(Configuration):
     ]
 
     WSGI_APPLICATION = 'course4_proj.wsgi.application'
+    
+    CELERY_RESULT_BACKEND = "django-db"
+    CELERY_BROKER_URL = "redis://localhost:6379/0"
 
 
     # Database
